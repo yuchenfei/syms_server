ef89d87af6d44bc12b476eec529e4b4da2b1088c
yuchenfei <yuchenfei19950319@gmail.com>
2018-08-29 11:28:10 +0800

    feat(file): 实现文件查询功能

--- apps/file/views.py
+++ apps/file/views.py
@@ -12,3 +12,10 @@ class FileViewSet(viewsets.ModelViewSet):
     parser_classes = (MultiPartParser,)
     authentication_classes = (CsrfExemptSessionAuthentication,)
     permission_classes = (permissions.IsAuthenticated,)
+
+    def get_queryset(self):
+        queryset = File.objects.all()
+        name = self.request.query_params.get('name', None)
+        if name:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
